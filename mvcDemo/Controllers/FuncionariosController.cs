b551e1f48d725bd192c308da2e781b11ef9f41e9
unisanta <unisanta@M216-20>
2023-10-27 21:14:14 -0300

    Add project files.

---
+++ mvcDemo/Controllers/FuncionariosController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvcDemo.Models;
+
+namespace mvcDemo.Controllers
+{
+    public class FuncionariosController : Controller
+    {
+        private readonly List<Funcionario> funcionarios = new List<Funcionario>();
+
+        public FuncionariosController()
+        {
+            funcionarios.Add(new Funcionario()
+            {
+                Nome = "Arthur Sampaio",
+                Cargo = "Desempregado",
+                CargaHorária = 0
+
+
+            });
+
+            funcionarios.Add(new Funcionario()
+            {
+                Nome = "Anthony Faccioli",
+                Cargo = "Desenvolvedor",
+                CargaHorária = 8
+                
+            });
+        }
+
+        public IActionResult Index()
+        {
+            return View(funcionarios);
+        }
+
+        public IActionResult Details(string id)
+        {
+            var funcionario = funcionarios.Find(a => a.Cargo == id);
+            if (funcionario == null) return NotFound();
+
+            return View(funcionario);
+        }
+    }
+}
---
+++ mvcDemo/Models/Funcionario.cs
@@ -0,0 +1,13 @@
+﻿namespace mvcDemo.Models
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; } = string.Empty;
+
+        public string Cargo { get; set; } = string.Empty;
+
+        public int CargaHorária  { get; set; }
+
+
+    }
+}
